b6382d5001f74b00ecb8017bbc89c4575635e22e
Josh Febbie <joshdfebbie@gmail.com>
2023-11-20 19:59:40 -0700

    mobile-sideB

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,12 +2,12 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { Goldman } from "next/font/google";
+import { Exo } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { LayoutDashboard } from "lucide-react"
+import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings } from "lucide-react"
 
-const goldman = Goldman({
-  weight: "700",
+const exo = Exo ({
+  weight: "500",
   subsets: ["latin"],
 });
 
@@ -18,22 +18,58 @@ const routes = [
   href: "/dashboard",
   color: "text-[#00ff6f]"
   },
+  {
+  lable: "Conversation",
+  icon: MessageSquare,
+  href: "/conversation",
+  color: "text-[#ff6cab]"
+  },
+  {
+  lable: "Image Generation",
+  icon: ImageIcon,
+  href: "/image",
+  color: "text-[#7a108a]"
+  },
+  {
+  lable: "Video Generation",
+  icon: VideoIcon,
+  href: "/video",
+  color: "text-[#10498a]"
+  },
+  {
+  lable: "Music Generation",
+  icon: Music,
+  href: "/music",
+  color: "text-[#7a108a]"
+  },
+  {
+  lable: "Code Generation",
+  icon: Code,
+  href: "/code",
+  color: "text-[#ff6cab]"
+  },
+  {
+  lable: "Settings",
+  icon: Settings,
+  href: "/settings",
+  color: "text-[#00ff6f]"
+  },
 ]
 
 const Sidebar = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-      <div className="px-2 py-1 flex-1">
+      <div className="px-3  flex-1">
         <Link
           href="/dashboard"
           className="flex
              items-center  mb-14"
         >
-          <div className="relative w-28 h-28 mr-4 rounded-lg">
+          <div className="relative w-24 h-24 mr-4 rounded-lg">
             <Image fill alt="Logo" src="/logo1.png" />
           </div>
-          <h1 className={cn("text-2xl font-bold text-[#00ff6f]",goldman.className)}>
-            PRODIGY.
+          <h1 className={cn("text-2xl font-bold text-[#00ff6f]", exo.className)}>
+            PRODI-G
           </h1>
         </Link>
         <div className="space-y-1">
@@ -41,9 +77,11 @@ const Sidebar = () => {
               <Link
                 href={route.href}
                 key={route.href}
+                className="flex items-center space-x-3 px-3 py-2 rounded-lg hover:bg-[#1F2937] transition duration-200 ease-in-out"
               >
                 <div className="flex items-center flex-1">
                   <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                  {route.lable}
                 </div>
               </Link>
             ))}
